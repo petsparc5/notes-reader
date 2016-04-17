23055b25049a65574af464a921a7b9cd2140652d
Peter Takacs <Peter_Takacs@epam.com>
2016-04-17 20:45:25 +0200

    Logger added to parser

--- src/main/java/com/senior/accelerator/notes_reader/parser/JsonNoteParser.java
+++ src/main/java/com/senior/accelerator/notes_reader/parser/JsonNoteParser.java
@@ -3,6 +3,8 @@ package com.senior.accelerator.notes_reader.parser;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +19,8 @@ import com.senior.accelerator.notes_reader.dao.RegularNote;
 public class JsonNoteParser {
 
     private static final String ERROR_MESSAGE = "Parsing error occurred with file: %s, message: %s";
+    private static final String LOG_MESSAGE = "Successfuly parsed: %s";
+    private final static Logger logger = LoggerFactory.getLogger(JsonNoteParser.class);
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -40,6 +44,7 @@ public class JsonNoteParser {
         Note parsedNote = null;
         try {
             parsedNote = objectMapper.readValue(file, noteType);
+            logger.debug(String.format(LOG_MESSAGE, fileName));
         } catch (IOException e) {
             String errorMessage = String.format(ERROR_MESSAGE, fileName, e.getMessage());
             throw new JsonNoteParseException(errorMessage, e);
